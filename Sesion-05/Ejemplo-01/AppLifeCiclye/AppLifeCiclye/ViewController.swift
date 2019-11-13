e2c7d4927940c16c38ee59d135128d8e0b4e0f72
Ricardo Montesinos <richimf1@gmail.com>
2019-11-13 10:00:40 -0600

    Sesion-05, code

---
+++ Sesion-05/Ejemplo-01/AppLifeCiclye/AppLifeCiclye/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppLifeCiclye
+//
+//  Created by Ricardo Montesinos on 13/11/19.
+//  Copyright © 2019 Bedu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
