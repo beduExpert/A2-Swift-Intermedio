29fd086512d71e3a7eb633b7ace37f79db6e0622
Ricardo Montesinos <richimf1@gmail.com>
2019-11-13 18:26:57 -0600

    Sesion-06, Ejemplo-02

---
+++ Sesion-06/Ejemplo-02/WeakReferences.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class MyObject {
+  init() {
+  }
+}
+
+var object: MyObject? = MyObject()
+weak var reference = object
+
+// set reference to nil
+object = nil
+
+if object == nil {
+  print("No hay memory leak")
+} else {
+  print("aun existe")
+}
+
+
